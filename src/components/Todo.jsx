46c2ae67efd3112e3056653f31088e1efbea7249
Sim-karta <simonenkonikita561@gmail.com>
2026-01-31 20:07:58 +0200

    add useState

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 import SearchTaskForm from "./SearchTaskForm";
 import TodoInfo from "./TodoInfo";
 import TodoList from "./TodoList";
 
 const Todo = () => {
-    const tasks = [
+    const [tasks, setTasks] = useState([
         {id: 'task-1', title: 'Купити молоко', isDone: false},
         {id: 'task-2', title: 'Погладити кота', isDone: true},
-    ]
+    ]);
+    const [newTaskTitle, setNewTaskTitle] = useState('');
 
     const deleteAllTasks = () => {
-        console.log('delete all tasks');
+        const isConfirmed = confirm('Are you sure you want to delete all tasks?');
+
+        if(isConfirmed) {
+            setTasks([]);
+        }
     }
 
     const deleteTask = (taskId) => {
-        console.log(`delete task: ${taskId}`);
+        setTasks(
+            tasks.filter((task) => task.id !== taskId)
+        )
     }
 
     const toggleTaskComplete = (taskId, isDone) => {
-        console.log(`Task: ${taskId} ${isDone ? "done" : "not done"}`);
+        setTasks(
+            tasks.map((task) => {
+                if(taskId === task.id) {
+                    return { ...task, isDone }
+                }
+
+                return task
+            })
+        )
     }
 
     const filterTasks = (query) => {
@@ -26,13 +42,26 @@ const Todo = () => {
     }
 
     const addTask = () => {
-        console.log('Task add');
+        if(newTaskTitle.trim().length > 0) {
+            const newTask = {
+                id: crypto?.randomUUID() ?? Date.now().toString(),
+                title: newTaskTitle,
+                isDone: false
+            }
+
+            setTasks([...tasks, newTask]);
+            setNewTaskTitle('');
+        }
     }
 
     return (
         <div className="todo">
             <h1 className="todo__title">To Do List</h1>
-            <AddTaskForm addTask={addTask}/>
+            <AddTaskForm 
+                addTask={addTask}
+                newTaskTitle={newTaskTitle}
+                setNewTaskTitle={setNewTaskTitle}
+            />
             <SearchTaskForm 
                 onSearchInput={filterTasks}
             />
